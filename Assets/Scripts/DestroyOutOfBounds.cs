4080e0ed14c17328fe189ab86c46f075bfdab4d8
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2020-11-29 11:24:42 +0800

    Making Progress
    Unit 2 part 1 and part 2 partially finished

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float top_bound = 30;
+    private float lower_bound = -10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > top_bound)
+        {
+            Destroy(gameObject);
+        } 
+        
+        else if (transform.position.z < lower_bound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontal_input;
+    public float speed = 10f;
+    public float xRange = 10f;
+    public GameObject projectile_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep the player inside the boundary we set
+        if (transform.position.x < (-xRange))
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontal_input = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * horizontal_input * Time.deltaTime * speed);
+        
+        // Launch a projectile from the player 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Variable for the function will be item, position and moving angle
+            Instantiate(projectile_prefab, transform.position, projectile_prefab.transform.rotation);
+        }
+    }
+}
